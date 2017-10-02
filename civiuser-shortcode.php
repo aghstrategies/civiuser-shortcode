6dc890d54f99f993f500ebcca9fcc98389fdf58e
Alice Frumin <alice@aghstrategies.com>
2017-10-02 15:05:59 -0400

    if not logged in

--- civiuser-shortcode.php
+++ civiuser-shortcode.php
@@ -11,38 +11,40 @@
 
 add_shortcode('civiuser_shortcode', 'civiuser_process_shortcode');
 civicrm_initialize();
-add_filter('widget_text','do_shortcode');
+add_filter('widget_text', 'do_shortcode');
 
 function civiuser_process_shortcode($attributes, $content = NULL) {
   // look up logged in contacts ID
   $userId = CRM_Core_Session::singleton()->getLoggedInContactID();
-  // get info of logged in contact thru the api
-  try {
-    $contactInfo = civicrm_api3('Contact', 'getsingle', array(
-      'id' => $userId,
-    ));
-  }
-  catch (CiviCRM_API3_Exception $e) {
-    $error = $e->getMessage();
-    CRM_Core_Error::debug_log_message(ts('API Error %1', array(
-      'domain' => 'civiuser-shortcode',
-      1 => $error,
-    )));
-  }
+  $userDiv = '<a href="<?php echo wp_login_url( get_permalink() ); ?>" title="Login">Login</a>';
+  if (!empty($userId)) {
+    // get info of logged in contact thru the api
+    try {
+      $contactInfo = civicrm_api3('Contact', 'getsingle', array(
+        'id' => $userId,
+      ));
+    }
+    catch (CiviCRM_API3_Exception $e) {
+      $error = $e->getMessage();
+      CRM_Core_Error::debug_log_message(ts('API Error %1', array(
+        'domain' => 'civiuser-shortcode',
+        1 => $error,
+      )));
+    }
 
-  // Create div of logged in contact's information
-  $userDiv = "
-  <div class='civiuser'>
-    <div>{$contactInfo['first_name']} {$contactInfo['last_name']}</div>
-    <div>{$contactInfo['street_address']}</div>
-    <div>{$contactInfo['supplemental_address_1']}</div>
-    <div>{$contactInfo['supplemental_address_2']}</div>
-    <div>{$contactInfo['supplemental_address_3']}</div>
-    <div>{$contactInfo['city']} {$contactInfo['state_province_name']}</div> <div>{$contactInfo['postal_code']}</div>
-    <div>{$contactInfo['phone']}</div>
-    <div>{$contactInfo['email']}</div>
-  </div>";
-
+    // Create div of logged in contact's information
+    $userDiv = "
+    <div class='civiuser'>
+      <div>{$contactInfo['first_name']} {$contactInfo['last_name']}</div>
+      <div>{$contactInfo['street_address']}</div>
+      <div>{$contactInfo['supplemental_address_1']}</div>
+      <div>{$contactInfo['supplemental_address_2']}</div>
+      <div>{$contactInfo['supplemental_address_3']}</div>
+      <div>{$contactInfo['city']} {$contactInfo['state_province_name']}</div> <div>{$contactInfo['postal_code']}</div>
+      <div>{$contactInfo['phone']}</div>
+      <div>{$contactInfo['email']}</div>
+    </div>";
+  }
   // print that div
   return "$userDiv";
 }
