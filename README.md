5f99e2e3749d3ada9d7278e5be807f7ea6759795
Alice Frumin <alice@aghstrategies.com>
2017-10-02 13:40:35 -0400

    adding readme

---
+++ README.md
@@ -0,0 +1,18 @@
+This Plugin:
+-----------
+
+Creates a shortcode `[civiuser_shortcode]`
+
+That displays the following contact information of the logged in wordpress user's linked civicrm contact (if these fields are populated)
+
+NOTE - address, phone and email will pull the primary records for the contact:
+
+'first_name' 'last_name'
+'street_address'
+'supplemental_address_1'
+'supplemental_address_2'
+'supplemental_address_3'
+'city' 'state_province_name'
+'postal_code'
+'phone'
+'email'
--- civiuser-shortcode.php
+++ civiuser-shortcode.php
@@ -1,35 +1,47 @@
-
 <?php
-   /*
-   Plugin Name: Civicrm User Shortcode
-   Plugin URI: http://aghstrategies.com
-   Description: a plugin to create a shortcode to display the information of the logged in wordpress users civicrm contact.
-   Version: 1.0
-   Author: AGH Strategies
-   Author URI: http://aghstrategies.com
-   License: GPL2
-   */
+ /*
+ Plugin Name: Civicrm User Shortcode
+ Plugin URI: http://aghstrategies.com
+ Description: a plugin to create a shortcode to display the information of the logged in wordpress users civicrm contact.
+ Version: 1.0
+ Author: AGH Strategies
+ Author URI: http://aghstrategies.com
+ License: GPL2
+ */
 
-   add_shortcode('civiuser_shortcode', 'civiuser_process_shortcode');
-   civicrm_initialize();
+add_shortcode('civiuser_shortcode', 'civiuser_process_shortcode');
+civicrm_initialize();
 
+function civiuser_process_shortcode($attributes, $content = NULL) {
+  // look up logged in contacts ID
+  $userId = CRM_Core_Session::singleton()->getLoggedInContactID();
+  // get info of logged in contact thru the api
+  try {
+    $contactInfo = civicrm_api3('Contact', 'getsingle', array(
+      'id' => $userId,
+    ));
+  }
+  catch (CiviCRM_API3_Exception $e) {
+    $error = $e->getMessage();
+    CRM_Core_Error::debug_log_message(ts('API Error %1', array(
+      'domain' => 'civiuser-shortcode',
+      1 => $error,
+    )));
+  }
 
-   function civiuser_process_shortcode($attributes, $content = NULL) {
-    //  wp_register_script('civipcp-widget-js', plugins_url('js/civipcp-widget.js', __FILE__), array('jquery', 'underscore'));
-    //  wp_enqueue_style('civipcp-widget-css', plugins_url('css/civipcp-widget.css', __FILE__));
-     try {
-      $event = civicrm_api3('Event', 'getsingle', array(
-        'return' => array("title"),
-        'id' => $page_id,
-      ));
-     }
-     catch (CiviCRM_API3_Exception $e) {
-       $error = $e->getMessage();
-       CRM_Core_Error::debug_log_message(ts('API Error %1', array(
-         'domain' => 'civiuser-shortcode',
-         1 => $error,
-       )));
-     }
-     $userDiv = "<div class='civiuser'></div>";
-     echo "$userDiv";
-   }
+  // Create div of logged in contact's information
+  $userDiv = "
+  <div class='civiuser'>
+    <div>{$contactInfo['first_name']} {$contactInfo['last_name']}</div>
+    <div>{$contactInfo['street_address']}</div>
+    <div>{$contactInfo['supplemental_address_1']}</div>
+    <div>{$contactInfo['supplemental_address_2']}</div>
+    <div>{$contactInfo['supplemental_address_3']}</div>
+    <div>{$contactInfo['city']} {$contactInfo['state_province_name']}</div> <div>{$contactInfo['postal_code']}</div>
+    <div>{$contactInfo['phone']}</div>
+    <div>{$contactInfo['email']}</div>
+  </div>";
+
+  // print that div
+  echo "$userDiv";
+}
